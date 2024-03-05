8f4e51208207b289bc56abc1db16a99f613096dc
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2024-03-05 20:48:12 -0300

    Add files via upload

---
+++ first.java/src/App.java
@@ -0,0 +1,7 @@
+// a little and simple project for start my journey coding in java
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!\n\nI'm Pedro and this was my first program in Java lenguage.");
+    }
+}
